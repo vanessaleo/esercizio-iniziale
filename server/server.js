ba79b4495932d96c12fbe5435f6860fecf372942
vanessaleo <vanessaleo3952@gmail.com>
2017-03-07 15:16:12 +0100

    creazione del server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
-var path = require('path');
-var router= express.Router();
+// var path = require('path');
+// var router= express.Router();
 
-
+app.get('/', function (req,res){
+  res.send('hello world');
+});
 app.listen(3500, function() {
     console.log('server partito su http://localhost:3500');
-})
+});
